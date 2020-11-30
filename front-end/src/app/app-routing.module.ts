b27f171a8df5f700894b5dd0439564ff1f198ca6
Grochowskij <59901129+Grochowskij@users.noreply.github.com>
2020-11-30 11:35:27 -0600

    Created Messaging component and recreated app.component.css file to be able to run the project.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MessagingComponent } from './messaging/messaging.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'messaging', component: MessagingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
