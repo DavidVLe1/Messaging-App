9f8b88e4c56621a55cfbce3d7a8a5e2af026e74a
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2020-12-05 18:47:14 -0600

    Add files via upload

--- front-end/src/app/_services/messaging.service.ts
+++ front-end/src/app/_services/messaging.service.ts
@@ -1,1 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagingService {
+
+  constructor(private httpClient: HttpClient ) { }
+
+  getAll(): Observable<any> {
+    return this.httpClient.get<any>(`${environment.apiUrl}/messaging`);
+  }
+
+  create(newUser): Observable<any> {
+    return this.httpClient.post<any>(`${environment.apiUrl}/messaging`, {users: newUser});
+  }
+
+}
