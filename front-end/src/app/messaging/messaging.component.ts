6dd3ce742f6fca87e5dd9e30e76142d651d3f600
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2020-12-05 18:46:56 -0600

    Add files via upload

--- front-end/src/app/messaging/messaging.component.ts
+++ front-end/src/app/messaging/messaging.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MessagingService} from '../_services/messaging.service';
+import {UserObject} from '../_models/user-object.model';
 @Component({
   selector: 'app-messaging',
   templateUrl: './messaging.component.html',
@@ -8,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 
 export class MessagingComponent implements OnInit {
 
+
+  // public newUsername;
+  // public usernames;
+  // public blankUsername = new UserObject('');
+  // constructor(private messagingService: MessagingService) { }
+  // This code above breaks the link to the messaging page when clicking the nav bar
   constructor() { }
 
   ngOnInit(): void {
+    // this.messagingService.getAll().subscribe(returnNames => {
+    //   this.usernames = returnNames.docs;
+    // });
   }
-
+  //
+  // saveUsername(): void {
+  //   this.messagingService.create(this.newUsername).subscribe( saveName => {
+  //     this.usernames.push(saveName);
+  //   });
+  // }
 }
